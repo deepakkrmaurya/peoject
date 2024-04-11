ff3fd0f2a21e80c52df5406d3b0249b2c38047cc
deepakkrmaurya <deepakmaurya2211@gmail.com>
2024-04-12 02:04:01 +0530

    import

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,10 @@
 'use client'
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { toast } from 'react-hot-toast'
-import { useRouter } from 'next/navigation'
-import Link from 'next/link'
+
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const page = () => {
   const router = useRouter();
